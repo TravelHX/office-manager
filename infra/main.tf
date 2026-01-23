7bbc37380c4e895c3b5bd351be7dfcaa2b06ace9
ildarmukhametzianovtravelhx <ildar.mukhametzianov@travelhx.com>
2026-01-23 16:36:39 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "rg_office_manager" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg_office_manager.name
+  location            = azurerm_resource_group.rg_office_manager.location
+
+  sku           = "Basic"
+  admin_enabled = true
+}
