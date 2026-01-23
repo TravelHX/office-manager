0ee53f947dfcbe3973ab0a5b82d45d0c27d80a26
ildarmukhametzianovtravelhx <ildar.mukhametzianov@travelhx.com>
2026-01-23 15:41:28 +0100

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,27 @@
+variable "subscription_id" {
+  type        = string
+}
+
+variable "client_id" {
+  type        = string
+}
+
+variable "client_secret" {
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+
+}
+
+variable "location" {
+  type        = string
+  default     = "Norway East"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "hx-office-manager-rg"
+}
