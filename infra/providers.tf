1a3115e2bb60fb30e8945c5dcf240e344806b7ae
ildarmukhametzianovtravelhx <ildar.mukhametzianov@travelhx.com>
2026-01-23 20:54:20 +0100

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "hx-office-manager-sa"
+    storage_account_name = "tfstateofficemanagersa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
