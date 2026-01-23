4f69811dfaf8d8ac4a800d9820530f503a1b0074
ildarmukhametzianovtravelhx <ildar.mukhametzianov@travelhx.com>
2026-01-23 16:38:42 +0100

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,16 @@ output "resource_group_name" {
 output "resource_group_location" {
   value = azurerm_resource_group.rg_office_manager.location
 }
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
