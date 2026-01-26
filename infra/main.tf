f041aeba86b16d0e3f19455f5e86c7f5416453dd
ildarmukhametzianovtravelhx <ildar.mukhametzianov@travelhx.com>
2026-01-26 15:17:27 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,43 @@ resource "azurerm_container_registry" "acr" {
   sku           = "Basic"
   admin_enabled = true
 }
+
+resource "azurerm_service_plan" "app_plan" {
+  name                = "office-manager-asp"
+  resource_group_name = azurerm_resource_group.rg_office_manager.name
+  location            = azurerm_resource_group.rg_office_manager.location
+
+  os_type  = "Linux"
+  sku_name = "B1" # cheap, fine for now
+}
+
+resource "azurerm_linux_web_app" "web_app" {
+  name                = "office-manager-app"
+  resource_group_name = azurerm_resource_group.rg_office_manager.name
+  location            = azurerm_resource_group.rg_office_manager.location
+  service_plan_id     = azurerm_service_plan.app_plan.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    application_stack {
+      docker_image_name   = "office-manager:latest"
+      docker_registry_url = "https://${azurerm_container_registry.acr.login_server}"
+    }
+
+    always_on = true
+  }
+
+  app_settings = {
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    DOCKER_REGISTRY_SERVER_URL          = "https://${azurerm_container_registry.acr.login_server}"
+  }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.web_app.identity[0].principal_id
+}
