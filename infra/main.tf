b40964f27125442fd91cfe63920304ac7008ec16
ildarmukhametzianovtravelhx <ildar.mukhametzianov@travelhx.com>
2026-01-26 17:27:22 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -46,8 +46,8 @@ resource "azurerm_linux_web_app" "web_app" {
   }
 }
 
-#resource "azurerm_role_assignment" "acr_pull" {
-  #scope                = azurerm_container_registry.acr.id
-  #role_definition_name = "AcrPull"
-  #principal_id         = azurerm_linux_web_app.web_app.identity[0].principal_id
-#}
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.web_app.identity[0].principal_id
+}
