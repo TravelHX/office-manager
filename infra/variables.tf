9f720680417bd232602b6754d03d38583f2d5a8a
ildarmukhametzianovtravelhx <ildar.mukhametzianov@travelhx.com>
2026-01-26 11:56:06 +0100

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,20 +1,3 @@
-variable "subscription_id" {
-  type        = string
-}
-
-variable "client_id" {
-  type        = string
-}
-
-variable "client_secret" {
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  type        = string
-
-}
 
 variable "location" {
   type        = string
