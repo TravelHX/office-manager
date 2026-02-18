b1be331e5613e7ddff57bc5555564e829f37269a
ildarmukhametzianovtravelhx <ildar.mukhametzianov@travelhx.com>
2026-02-18 18:19:41 +0100

    Update main.js

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,6 +1,6 @@
 // Office Manager Main JavaScript
 
-const API_BASE_URL = 'https://office-manager-app.azurewebsites.net';
+const API_BASE_URL = 'https://hx-hub-d-office-manager-app.azurewebsites.net';
 
 // Authentication functions
 function getAuthToken() {
